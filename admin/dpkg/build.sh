39db4d44c341821fa810971f5d3f77909170e3d7
"Andrew Stormont ext:(%22) <andyjstormont@googlemail.com>
2011-06-11 14:09:10 +0100

    Fix dpkg FTBFS on solaris

--- admin/dpkg/build.sh
+++ admin/dpkg/build.sh
@@ -10,13 +10,44 @@ PKGNAME=dpkg
 PKGVERS=1.15.8.10
 PKGARCH="$PKGNAME"_"$PKGVERS.tar.bz2"
 PKGDSC="$PKGNAME"_"$PKGVERS.dsc"
+PKGDIR="$PKGNAME-$PKGVERS"
 
-mkdir -p build && cd build
-wget -c $WEBARCHIVE_PATH/$PKGARCH
-wget -c $WEBARCHIVE_PATH/$PKGDSC
+if [ -d build ]; then
+  echo "Removing old build dir"
+  rm -rf build
+fi
 
-tar -xvf "$PKGARCH" --exclude "debian" 
+mkdir -p build cache && cd cache
 
-cp -r ../debian "$PKGNAME-$PKGVERS/"
+if [ -f "$PKGARCH" ]; then
+  echo "Using local $PKGARCH"
+else
+  echo "Downloading $PKGARCH ...\c"
+  wget -q -c $WEBARCHIVE_PATH/$PKGARCH
+  echo " done"
+fi
 
-cd "$PKGNAME-$PKGVERS" && dpkg-buildpackage -sa -uc -us
+if [ -f "$PKGDSC" ]; then
+  echo "Using local $PKGDSC"
+else
+  echo "Downloading $PKGDSC ...\c"
+  wget -q -c $WEBARCHIVE_PATH/$PKGDSC
+  echo " done"
+fi
+
+cd ../build
+
+echo "Unpacking upstream source ...\c"
+tar -xf "../cache/$PKGARCH" --exclude "debian"
+echo " done" 
+
+echo "Creating $PKGNAME"_"$PKGVERS.orig.tar.bz2 ...\c"
+tar -cjf "$PKGNAME"_"$PKGVERS.orig.tar.bz2" "$PKGDIR"
+echo " done"
+
+echo "Applying our changes ...\c"
+cp -r ../debian "$PKGDIR/" && cd "$PKGDIR"
+QUILT_PATCHES=debian/patches quilt push -a
+echo " done"
+
+DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -sa -uc -us
