06edf4d16ea918932b69ed7cf98e978adcc19f15
Andrew Stormont <andyjstormont@googlemail.com>
2011-06-29 21:07:02 +0100

    re #5603 rb1015 Rebase dpkg on 1.15.8.11

--- src/dpkg/build.sh
+++ src/dpkg/build.sh
@@ -7,7 +7,7 @@ set -e
 WEBARCHIVE_PATH="http://ftp.uk.debian.org/debian/pool/main/d/dpkg/"
 
 PKGNAME=dpkg
-PKGVERS=1.15.8.10
+PKGVERS=`grep Upstream-Version debian/control | sed 's/Upstream-Version:\s//'`
 PKGARCH="$PKGNAME"_"$PKGVERS.tar.bz2"
 PKGORIG="$PKGNAME"_"$PKGVERS.orig.tar.bz2"
 PKGDSC="$PKGNAME"_"$PKGVERS.dsc"
@@ -24,16 +24,24 @@ if [ -f "$PKGARCH" ]; then
   echo "Using local $PKGARCH"
 else
   echo "Downloading $PKGARCH ...\c"
-  wget -q -c $WEBARCHIVE_PATH/$PKGARCH
-  echo " done"
+  if wget -q -c $WEBARCHIVE_PATH/$PKGARCH; then
+    echo " done"
+  else
+    echo " failed"
+    exit 1
+  fi
 fi
 
 if [ -f "$PKGDSC" ]; then
   echo "Using local $PKGDSC"
 else
   echo "Downloading $PKGDSC ...\c"
-  wget -q -c $WEBARCHIVE_PATH/$PKGDSC
-  echo " done"
+  if wget -q -c $WEBARCHIVE_PATH/$PKGDSC; then
+    echo " done"
+  else
+    echo " failed"
+    exit 1
+  fi
 fi
 
 cd ../build
