53e1aa85ff8ed85be0a7e9f54a8d9c4b860514fc
Alexander Eremin <a.eremin@nexenta.com>
2011-07-01 19:03:50 -0700

    re #5629 rb1027 Remove libintl.so.8 from pkg-config

--- src/pkg-config/build.sh
+++ src/pkg-config/build.sh
@@ -1,17 +1,44 @@
 #!/bin/sh
 
+set -e
+
 # build script for pkg-config
 
-WEBARCHIVE_PATH="http://apt.nexenta.org/wip/dists/siddy-testing/main/source/devel"
+WEBARCHIVE_PATH="http://ftp.fi.debian.org/debian/pool/main/p/pkg-config"
 
 PKGNAME="pkg-config"
-PKGVER="0.25"
-PKGARCH="$PKGNAME"_"$PKGVER".orig.tar.gz
+PKGVERS="0.25"
 
-mkdir build && cd build && wget $WEBARCHIVE_PATH/$PKGARCH
+PKGORIG="$PKGNAME"_"$PKGVERS.orig.tar.gz"
+PKGDSC="$PKGNAME"_"$PKGVERS"-"1.1.dsc"
+PKGDIR="$PKGNAME-$PKGVERS"
 
-tar xzvf $PKGARCH --exclude "debian" 
+if [ -d build ]; then
+  echo "Removing old build dir"
+  rm -rf build
+fi
 
-cp -r ../debian $PKGNAME-$PKGVER/
+mkdir -p build cache && cd cache
 
-cd $PKGNAME-$PKGVER && dpkg-buildpackage -sa -uc -us
+  echo "Downloading $PKGORIG ...\c"
+  echo $WEBARCHIVE_PATH/$PKGORIG
+  wget -q -c $WEBARCHIVE_PATH/$PKGORIG
+  echo " done"
+
+  echo "Downloading $PKGDSC ...\c"
+  wget -q -c $WEBARCHIVE_PATH/$PKGDSC
+  echo " done"
+
+cd ../build
+
+  echo "Unpacking $PKGORIG ...\c"
+  tar -xzf "../cache/$PKGORIG"
+  echo " done"
+
+echo "Applying our changes ...\c"
+cp "../cache/$PKGORIG" .
+cp -r ../debian "$PKGDIR/" && cd "$PKGDIR"
+
+echo " done"
+
+DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -sa -uc -us
