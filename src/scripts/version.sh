e220dcd9025f941ad9f75437ce85be7b97df731d
Kartik Mistry <kartik.mistry@nexenta.com>
2011-07-19 18:12:38 +0530

    Added multi-package way

--- src/scripts/version.sh
+++ src/scripts/version.sh
@@ -7,14 +7,22 @@
 #
 #To Do: Use sane ways to determine siddy versions.
 
-cd packaging/src/$1
-HGVER=`dpkg-parsechangelog --count 1 | grep "Version:"`
-SQUVER=`rmadison -s squeeze $1 | cut -d "|" -f 2`
-SIDVER1=`apt-cache show $1|grep -s Filename:|cut -d "/" -f 6|cut -d "_" -f 2|head -1`
-SIDVER2=`apt-cache show $1|grep -s Filename:|cut -d "/" -f 6|cut -d "_" -f 2|tail -1`
+for i in $*
+do
 
-echo "$1 has following versions:"
-echo "HG repo is at: $HGVER"
-echo "Squeeze is at:$SQUVER"
-echo "Siddy-testing is at:" $SIDVER1
-echo "Siddy-unstable is at:" $SIDVER2
+ cd packaging/src/$i
+ HGVER=`dpkg-parsechangelog --count 1 | grep "Version:"`
+ SQUVER=`rmadison -s squeeze $i | cut -d "|" -f 2`
+ SIDVER1=`apt-cache show $i|grep -s Filename:|cut -d "/" -f 6|cut -d "_" -f 2|head -1`
+ SIDVER2=`apt-cache show $i|grep -s Filename:|cut -d "/" -f 6|cut -d "_" -f 2|tail -1`
+
+ echo "$i has following versions:"
+ echo "HG repo is at: $HGVER"
+ echo "Squeeze is at:$SQUVER"
+ echo "Siddy-testing is at:" $SIDVER1
+ echo "Siddy-unstable is at:" $SIDVER2
+ echo "-------------------------------"
+
+ cd -
+
+done
