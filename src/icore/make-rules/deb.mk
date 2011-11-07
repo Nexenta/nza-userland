3cac0408779ec9f3365471c67654fe3b3a87097c
Igor Pashev <igor.pashev@nexenta.com>
2011-11-07 13:19:07 +0300

    Added nabat-upload and nabat-push targets

--- src/icore/make-rules/deb.mk
+++ src/icore/make-rules/deb.mk
@@ -12,7 +12,12 @@ PROTO_DIRS = $(PKG_PROTO_DIRS:%=-d %)
 # and debs (like debian/pkg-name)
 DEBS_DIR = $(PROTO_DIR)/debs
 
-deb: build install $(MANGLED) $(DEBMAKER)
+
+# since build and install target are phony
+# 'make deb' will always run and
+# this is ok to make sure we do not have
+# garbage in $(DEBS_DIR):
+deb: $(DEBMAKER) $(MANGLED) build install
 	rm -rf $(DEBS_DIR)
 	$(MKDIR) $(DEBS_DIR)
 	$(DEBMAKER) \
@@ -21,5 +26,24 @@ deb: build install $(MANGLED) $(DEBMAKER)
 		-V $(DEBVERSION) \
 		-D $(DEBS_DIR) \
 		$(PROTO_DIRS) $(MANGLED)
+
+NABAT_HOST    ?= 10.3.10.2
+NABAT_USER    ?= changeme_by_NABAT_USER
+NABAT_DIST    ?= base
+NABAT_REPO    ?= unstable
+NABAT_DESTDIR ?= pkg_upload
+
+nabat-upload:
+	@if ls $(DEBS_DIR)/*.deb 2>/dev/null 1>/dev/null ; then \
+		echo uploading to $(NABAT_USER)@$(NABAT_HOST):$(NABAT_DESTDIR)/$(NABAT_DIST)-$(NABAT_REPO)/ ; \
+		scp $(DEBS_DIR)/*.{deb,changes} \
+			$(NABAT_USER)@$(NABAT_HOST):$(NABAT_DESTDIR)/$(NABAT_DIST)-$(NABAT_REPO)/ ; \
+		touch $@ ; \
+	else \
+		echo "make deb first" ; \
+	fi
+
+nabat-push:
+	ssh $(NABAT_USER)@$(NABAT_HOST) "touch '$(NABAT_DESTDIR)/$(NABAT_DIST)-$(NABAT_REPO)/packages.fini'"
 
 
