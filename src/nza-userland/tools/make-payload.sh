23bc8eda80b49892423f2785f958daffdc9d31c7
Igor Pashev <igor.pashev@nexenta.com>
2012-03-14 05:42:19 +0400

    make-payload.sh: replace amd64 with $(MACH64)

--- src/nza-userland/tools/make-payload.sh
+++ src/nza-userland/tools/make-payload.sh
@@ -14,9 +14,11 @@ elif [ $# != 0 ]; then
     usage
 fi
 
+(
 gfind * -type d -printf 'dir path=%p\n'
 gfind * -type f -printf 'file path=%p\n'
 gfind * -type l -printf 'link path=%p target=%l\n'
+) | sed -e 's,lib/amd64,lib/$(MACH64),' -e 's,bin/amd64,bin/$(MACH64),'
 
 exit 0
 
