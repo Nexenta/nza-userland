7a8835b6b6b16b1b48e601cd61512f9fdc758afb
Igor Pashev <igor.pashev@nexenta.com>
2012-10-29 15:49:11 +0400

    Fixed postinst scripts; updated copyright

--- nza-userland/tools/debmaker.pl
+++ nza-userland/tools/debmaker.pl
@@ -1,6 +1,27 @@
 #!/usr/bin/env perl
 
-# Copyright (c) 2012, Nexenta Systems, Inc. All rights reserved.
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# You can obtain a copy of the license
+# at http://www.opensource.org/licenses/CDDL-1.0
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each file.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright (C) 2012, Nexenta Systems, Inc. All rights reserved.
+#
 
 use 5.010;
 use strict;
@@ -696,7 +717,7 @@ foreach my $manifest_file (@ARGV) {
             }
         }
         if (@hl_script) {
-            $postinst .= 'if [ "$1" = install ] || [ "$1" = upgrade ]; then' . "\n";
+            $postinst .= 'if [ "$1" = configure ]; then' . "\n";
             $postrm  .= 'if [ "$1" = remove ]; then' . "\n";
             foreach my $l (@hl_script) {
                 my $d = dirname $$l{path};   $d = "/$d" unless $d =~ /^\//;
@@ -704,7 +725,7 @@ foreach my $manifest_file (@ARGV) {
                 my $p = $$l{'path'};         $p = "/$p" unless $p =~ /^\//;
                 my $t = $$l{'target'};
                 $postinst .= " if ! [ -f \${BASEDIR}$p ]; then\n";
-                $postinst .= "  (cd \${BASEDIR}$d && ln $t $b) || true\n";
+                $postinst .= "  cd \${BASEDIR}$d && ln $t $b || true\n";
                 $postinst .= " fi\n";
                 $postrm  .= " rm $p || true\n";
             }
